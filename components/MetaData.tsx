51d2642ad1e2ff05f3b109e0e948cca5f844f7c2
Eliezer Basubi <eliezer.basubi30@gmail.com>
2022-03-19 12:23:14 +0300

    Add dynamic metatags

--- components/MetaData.tsx
+++ components/MetaData.tsx
@@ -10,40 +10,13 @@ const MetaData = ({ metatag }: Props) => {
   return (
     <Head>
       <title>{metatag?.title}</title>
-      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
       <meta name="keywords" content={metatag?.keyWords} key="keywords" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
 
       <meta
         name="description"
         content={metatag?.description}
         key="description"
       />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/icons/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/icons/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/icons/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/icons/site.webmanifest" />
-      <link
-        rel="mask-icon"
-        href="/icons/safari-pinned-tab.svg"
-        color="#5bbad5"
-      />
-      <meta name="msapplication-TileColor" content="#2b5797" />
-      <meta name="theme-color" content="#ffffff" />
 
       {/* <!-- Primary Meta Tags --> */}
 
