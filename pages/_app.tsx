7c77c40516a84d6a22638ab881899aa21c7e9004
Eliezer Basubi <eliezer.basubi30@gmail.com>
2022-03-25 18:57:01 +0300

    Add nprogress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
+import NProgress from 'nprogress';
+import { Router } from 'next/router';
+
+import 'nprogress/nprogress.css';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  NProgress.configure({
+    minimum: 0.3,
+    easing: 'ease',
+    speed: 800,
+    showSpinner: false,
+  });
+
+  Router.events.on('routeChangeStart', () => NProgress.start());
+  Router.events.on('routeChangeComplete', () => NProgress.done());
+  Router.events.on('routeChangeError', () => NProgress.done());
+
   return (
     <RecoilRoot>
       <Component {...pageProps} />
