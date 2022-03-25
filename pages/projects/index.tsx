c14bb57dfff7171de9c17a09ffe18bc0048ab8c3
Eliezer Basubi <eliezer.basubi30@gmail.com>
2022-03-25 21:05:29 +0300

    Improve projects activity page

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -21,14 +21,14 @@ const Projects = ({ metatag, projects }: Props) => {
       <div className="w-full">
         <div className="w-full p-4 lg:p-8 xl:py-8 xl:px-24">
           <PageHeader />
-          <div className="w-full lg:max-w-4xl 2xl:max-w-7xl mx-auto">
+          <div className="w-full md:max-w-lg lg:max-w-4xl 2xl:max-w-7xl mx-auto">
             <div className="min-h-screen py-12">
               <SectionHeader
                 title="Projects"
-                description="Enjoy my collected articles on different topics around blockchain, web performance, UI/UX design and many more."
+                description="Some of the projects I have worked on!"
                 descriptionClassName="md:text-md 2xl:text-xl 5xl:text-3xl font-normal mt-4 text-center"
               />
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-12">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6 my-12">
                 {projects.map((project) => (
                   <ProjectCard key={project._id} project={project} />
                 ))}
